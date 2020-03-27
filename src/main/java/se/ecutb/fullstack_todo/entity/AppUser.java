077f74f83aaa5a1cccf0f062bdfa4a19a5dff7a7
Peter Sundberg <peter.sundberg67@gmail.com>
2020-03-27 10:52:10 +0100

    Created AppUserRole class

--- src/main/java/se/ecutb/fullstack_todo/entity/AppUser.java
+++ src/main/java/se/ecutb/fullstack_todo/entity/AppUser.java
@@ -1,10 +1,17 @@
 package se.ecutb.fullstack_todo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity
 public class AppUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
     private String username;
     private String firstName;
---
+++ src/main/java/se/ecutb/fullstack_todo/entity/AppUserRole.java
@@ -0,0 +1,62 @@
+package se.ecutb.fullstack_todo.entity;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class AppUserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleId;
+    @Column(unique = true)
+    private String role;
+
+    public AppUserRole(String role) {
+        this.role = role;
+    }
+
+    public AppUserRole() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUserRole that = (AppUserRole) o;
+        return roleId == that.roleId &&
+                Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId, role);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUserRole{" +
+                "role='" + role + '\'' +
+                '}';
+    }
+
+
+}
