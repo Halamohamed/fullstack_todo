52c5b45c7e7a94f7605193b4a541137e194b2d8c
Hala Mohammed <halaalimohammed@gmail.com>
2020-03-27 10:29:48 +0100

    created AppUser

---
+++ src/main/java/se/ecutb/fullstack_todo/entity/AppUser.java
@@ -0,0 +1,102 @@
+package se.ecutb.fullstack_todo.entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AppUser {
+
+    private int userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private LocalDate regDate;
+    private String password;
+
+    public AppUser(String username, String firstName, String lastName, LocalDate regDate, String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.regDate = regDate;
+        this.password = password;
+    }
+
+    public AppUser() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return userId == appUser.userId &&
+                Objects.equals(username, appUser.username) &&
+                Objects.equals(firstName, appUser.firstName) &&
+                Objects.equals(lastName, appUser.lastName) &&
+                Objects.equals(regDate, appUser.regDate) &&
+                Objects.equals(password, appUser.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, firstName, lastName, regDate, password);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", regDate=" + regDate +
+                '}';
+    }
+}
